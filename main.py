b4b086ea2fb2fed3ffec69cd6c33a6db6de930fb
Kirsten-Holm <dj4kbox@gmail.com>
2025-06-03 12:32:49 +0200

    added method that actually makes a maze out of the grid, i'm quite confident that it works and is scalable

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from maze import *
 def main ():
     
     win = Window(800,600)
-    maze = Maze(10,10,5,5,25,30,win)
+    maze = Maze(10,10,8,8,30,30,win)
 
-    maze._Maze__break_entrance_and_exit()
-    
-    maze._Maze__cells[2][2].draw_move(maze._Maze__cells[4][4])
+
     
     win.wait_for_close()
     
--- maze.py
+++ maze.py
@@ -32,6 +32,8 @@ class Maze:
         #non parameter values
         self.__cells = []
         self.__create_cells()
+        self.__break_entrance_and_exit()
+        self.__break_walls_r(0,0)
         
     def __create_cells(self):
         
@@ -47,17 +49,17 @@ class Maze:
         
     def __draw_cell(self, i, j):
         x_cord = self.x1
-        if j != 0:
-            x_cord = self.cell_size_x * j + self.x1
-        y_cord = self.y1
         if i != 0:
-            y_cord = self.cell_size_y * i + self.y1
+            x_cord = self.cell_size_x * i + self.x1
+        y_cord = self.y1
+        if j != 0:
+            y_cord = self.cell_size_y * j + self.y1
             
         if self.win == None:
             self.__cells[i][j].__x1 = x_cord
-            self.__cells[i][j].__x1 = y_cord
-            self.__cells[i][j].__x1 = x_cord + self.cell_size_x
-            self.__cells[i][j].__x1 = x_cord + self.cell_size_y
+            self.__cells[i][j].__y1 = y_cord
+            self.__cells[i][j].__x2 = x_cord + self.cell_size_x
+            self.__cells[i][j].__y2 = x_cord + self.cell_size_y
         else:
             self.__cells[i][j].draw(x_cord,y_cord,x_cord+self.cell_size_x,y_cord+self.cell_size_y)
             self.animate()
@@ -83,7 +85,51 @@ class Maze:
         self.__cells[i][j].visited = True
         
         while True:
-            nodes_to_visit = []
-            self.__cells[i]
+            coords_to_visit = []
+            if i + 1 < self.num_cols:
+                if self.__cells[i+1][j].visited == False:
+                    coords_to_visit.append((i + 1, j))
+            if j + 1 < self.num_rows:
+                if self.__cells[i][j+1].visited == False:
+                    coords_to_visit.append((i, j + 1))
+            if i - 1 >= 0:
+                if self.__cells[i-1][j].visited == False:
+                    coords_to_visit.append((i - 1, j))
+            if j - 1 >= 0:
+                if self.__cells[i][j-1].visited == False:
+                    coords_to_visit.append((i, j - 1))
+                    
+            if len(coords_to_visit) == 0:
+                return
+            
+            next_cell = coords_to_visit[random.randrange(0,len(coords_to_visit))]
+            next_i = next_cell[0]
+            next_j = next_cell[1]
+            if next_i == i + 1:
+                self.__cells[i][j].has_right_wall = False
+                self.__draw_cell(i,j)
+                self.__cells[next_i][next_j].has_left_wall = False
+                self.__draw_cell(next_i,next_j)
+            if next_i == i - 1:
+                self.__cells[i][j].has_left_wall = False
+                self.__draw_cell(i,j)
+                self.__cells[next_i][next_j].has_right_wall = False
+                self.__draw_cell(next_i,next_j)
+            if next_j == j + 1:
+                self.__cells[i][j].has_bottom_wall = False
+                self.__draw_cell(i,j)
+                self.__cells[next_i][next_j].has_top_wall = False
+                self.__draw_cell(next_i,next_j)
+            if next_j == j - 1:
+                self.__cells[i][j].has_top_wall = False
+                self.__draw_cell(i,j)
+                self.__cells[next_i][next_j].has_bottom_wall = False
+                self.__draw_cell(next_i,next_j)
+                
+            self.__break_walls_r(next_i,next_j)
+
+                
+
+            
         
         
